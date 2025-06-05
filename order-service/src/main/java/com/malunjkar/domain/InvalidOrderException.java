1c724781e375acf2ed34795fae189b00d0257484
adeshmalunjkar <adesh.malunjkar@upgrad.com>
2025-06-05 23:33:12 +0530

    Add order service with database and messaging configuration

---
+++ order-service/src/main/java/com/malunjkar/domain/InvalidOrderException.java
@@ -0,0 +1,8 @@
+package com.malunjkar.domain;
+
+public class InvalidOrderException extends RuntimeException {
+
+    public InvalidOrderException(String message) {
+        super(message);
+    }
+}
---
+++ order-service/src/test/java/com/malunjkar/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malunjkar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(ContainerConfig.class)
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
